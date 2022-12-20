cfa07e2d6ccc4472d73dafbe7e2541580808a4a8
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2022-12-21 02:05:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//Please put your answer here
+
+$("#signup-modal").hide();
+
+$(".signup-show").click(function(){
+    $("#signup-modal").show();
+});
+
+$("#close-modal").click(function(){
+    $("#signup-modal").hide();
+});
